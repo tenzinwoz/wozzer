6f49b7fc55334ea0f0a3c0f5c1b57c709195ee1d
tenzin woeser <tenzinwoeser15@gmail.com>
2023-04-09 21:56:05 -0400

    Added hasnode links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,21 @@ import Eyestar from "../../assets/images/eye_star.svg";
 export default function Footer() {
   return (
     <div className="footer container">
+      <div>Designed & Developed by Tenzin Woeser</div>
       <div>
-        <div>
-          <img src={Eyestar} alt="Wozzer" />
-        </div>
-        Designed & Developed by Tenzin Woeser
+        <img src={Eyestar} alt="Wozzer" />
+      </div>
+      <div>
+        Check out my tech blogs
+        <a
+          href="https://tenzinwoz.hashnode.dev/"
+          className="yellow-text"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          HERE
+        </a>
       </div>
     </div>
   );
