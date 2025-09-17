c530f8da820905a6a68dbf433ca6add80af95a44
Tenzin Woeser <tenzin.woeser@swish.ca>
2025-09-17 16:36:47 -0400

    Refactor Experience component: Update company tabs and job descriptions, enhance accessibility features, and improve code consistency

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,15 +11,27 @@ export default function Experience() {
             </h1>
           </div>
           <div className="row">
-            <div class="d-flex align-items-start mobile-nav-pills">
+            <div className="d-flex align-items-start mobile-nav-pills">
               <div
-                class="nav flex-column nav-pills me-3 experience-tab-nav"
+                className="nav flex-column nav-pills me-3 experience-tab-nav"
                 id="v-pills-tab"
                 role="tablist"
                 aria-orientation="vertical"
               >
                 <button
-                  class="nav-link active company-name "
+                  className="nav-link active company-name "
+                  id="v-pills-swish-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#v-pills-swish"
+                  type="button"
+                  role="tab"
+                  aria-controls="v-pills-swish"
+                  aria-selected="true"
+                >
+                  Swish
+                </button>
+                <button
+                  className="nav-link company-name "
                   id="v-pills-rbc-tab"
                   data-bs-toggle="pill"
                   data-bs-target="#v-pills-rbc"
@@ -31,7 +43,7 @@ export default function Experience() {
                   RBC
                 </button>
                 <button
-                  class="nav-link company-name "
+                  className="nav-link company-name "
                   id="v-pills-neosoft-tab"
                   data-bs-toggle="pill"
                   data-bs-target="#v-pills-neosoft"
@@ -42,39 +54,50 @@ export default function Experience() {
                 >
                   Neosoft
                 </button>
-                <button
-                  class="nav-link company-name"
-                  id="v-pills-resoluteai-tab"
-                  data-bs-toggle="pill"
-                  data-bs-target="#v-pills-resoluteai"
-                  type="button"
-                  role="tab"
-                  aria-controls="v-pills-resoluteai"
-                  aria-selected="false"
-                >
-                  ResoluteAI
-                </button>
-
-                <button
-                  class="nav-link company-name"
-                  id="v-pills-blufig-tab"
-                  data-bs-toggle="pill"
-                  data-bs-target="#v-pills-blufig"
-                  type="button"
-                  role="tab"
-                  aria-controls="v-pills-blufig"
-                  aria-selected="false"
-                >
-                  Blufig
-                </button>
               </div>
-              <div class="tab-content" id="v-pills-tabContent">
+              <div className="tab-content" id="v-pills-tabContent">
                 <div
-                  class="tab-pane fade show active"
+                  className="tab-pane fade show active"
+                  id="v-pills-swish"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-swish-tab"
+                  tabIndex="0"
+                >
+                  <h2 className="sub-heading">Fullstack developer</h2>
+                  <p className="duration">
+                    Nov 2023 - Present <span> (Canada)</span>
+                  </p>
+                  <ul>
+                    <li>
+                      Worked collaboratively in a team to rewrite the quoting
+                      application using Next.js and Nest.js, resulting in
+                      significant improvements in application speed and
+                      developer efficiency.
+                    </li>
+                    <li>
+                      Implemented Redis caching in the product pricing API to
+                      significantly improve response times and reduce database
+                      load.
+                    </li>
+                    <li>
+                      Integrated third-party vendor software, including Customer
+                      Gauge and Harbr (credit check provider), into enterprise
+                      systems by leveraging REST APIs and implementing webhooks
+                      to streamline data exchange and automation.
+                    </li>
+                    <li>
+                      Assisted the IT team in reducing costs by migrating
+                      applications to a more cost-effective serverless
+                      architecture such as Vercel and Cloud Run (GCP)
+                    </li>
+                  </ul>
+                </div>
+                <div
+                  className="tab-pane fade"
                   id="v-pills-rbc"
                   role="tabpanel"
                   aria-labelledby="v-pills-rbc-tab"
-                  tabindex="0"
+                  tabIndex="0"
                 >
                   <h2 className="sub-heading">Fullstack developer(coop)</h2>
                   <p className="duration">
@@ -87,18 +110,23 @@ export default function Experience() {
                       and Axe, and provided technical solutions.
                     </li>
                     <li>
-                      Developed full-stack application automating testing for
-                      enterprise websites/apps, providing accessibility scores,
-                      and assisting in identifying accessibility issues.
+                      Used MERN (MongoDB, Express, React, Node) stack to build
+                      and deliver an internal testing application providing
+                      accessibility scores, and assisting in identifying
+                      accessibility issues for more than 100 websites/apps.
+                    </li>
+                    <li>
+                      Used GitHub for source code management and integrated git
+                      hub actions for application deployment.
                     </li>
                   </ul>
                 </div>
                 <div
-                  class="tab-pane fade"
+                  className="tab-pane fade"
                   id="v-pills-neosoft"
                   role="tabpanel"
                   aria-labelledby="v-pills-neosoft-tab"
-                  tabindex="0"
+                  tabIndex="0"
                 >
                   <h2 className="sub-heading">React Developer</h2>
                   <p className="duration">
@@ -106,84 +134,17 @@ export default function Experience() {
                   </p>
                   <ul>
                     <li>
-                      Developed UI components that adhered to the app theme,
-                      deploying essential features like user profile and
-                      dashboard, allowing creation, update, and deletion of
-                      profiles. The dashboard had eight pages with 10
-                      sub-components and eight graph types. Employed React hooks
-                      for form handling, conditional rendering, and converted
-                      class components for cleaner code.
+                      Built reusable React UI components (50+), dashboards, and
+                      profile modules with Redux state management.
                     </li>
                     <li>
-                      Based on my communication and leadership abilities, I was
-                      selected among my team of six to represent NeoSoft in
-                      interviewing 16 students from eight colleges for a junior
-                      JavaScript developer position
+                      Collaborated with cross-functional teams to standardize
+                      developer tooling, improving delivery speed and
+                      consistency.
                     </li>
                     <li>
-                      Global state created with Redux to handle app states such
-                      as profile and dashboard features. Three reducer files
-                      with 20+ states, three action files with 40+ methods,
-                      including error handling
-                    </li>
-                  </ul>
-                </div>
-                <div
-                  class="tab-pane fade"
-                  id="v-pills-resoluteai"
-                  role="tabpanel"
-                  aria-labelledby="v-pills-resoluteai-tab"
-                  tabindex="0"
-                >
-                  <h2 className="sub-heading">React Developer (Intern)</h2>
-                  <p className="duration">
-                    Nov 2020 - April 2021 <span> (India)</span>
-                  </p>
-                  <ul>
-                    <li>
-                      From scratch, I designed and implemented a Firebase
-                      database structure for an AI software, including three
-                      models with over 10 sub-collections each. Additionally, I
-                      created two RDBMS models for real-time data syncing, and
-                      incorporated Firebase's authentication service. I
-                      determined the database structure based on business
-                      requirements.
-                    </li>
-                    <li>
-                      Used ContextAPI to create four state provider wrappers for
-                      different features. Each wrapper had over 10 methods. Used
-                      ContextAPI with React useReducer to power the small web
-                      app to the maximum
-                    </li>
-                  </ul>
-                </div>
-
-                <div
-                  class="tab-pane fade"
-                  id="v-pills-blufig"
-                  role="tabpanel"
-                  aria-labelledby="v-pills-blufig-tab"
-                  tabindex="0"
-                >
-                  <h2 className="sub-heading">Wordpress Developer </h2>
-                  <p className="duration">
-                    July 2018 - Feb 2020 <span> (India)</span>
-                  </p>
-                  <ul>
-                    <li>
-                      Developed a custom WordPress theme that improved website
-                      speed by 30%, using HTML and Sass and removing all page
-                      builders. Additionally, I implemented necessary SEO
-                      requirements prior to deploying the website powered by
-                      custom theme.
-                    </li>
-                    <li>
-                      Developed four SEO-friendly websites for different
-                      companies across various sectors, using WordPress with
-                      Elementor. Adhering to all SEO guidelines, I ensured the
-                      websites featured proper content hierarchy, alt tags,
-                      sitemaps, and accurately replicated their respective
-                      designs
+                      Represented the company in developer interviews and
+                      mentorship for new hires.
                     </li>
                   </ul>
                 </div>
